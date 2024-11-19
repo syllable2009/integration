8a6ca551c78611ac328bb89f49edc7ea8d3f9b76
jiaxiaopeng <jiaxiaopeng@kuaishou.com>
2024-11-19 14:46:08 +0800

    update:save

--- integration-base/src/main/java/com/jxp/integration/response/ExceptionCode.java
+++ integration-base/src/main/java/com/jxp/integration/response/ExceptionCode.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD, ElementType.TYPE}) // 可以用于方法和类
+@Target({ElementType.FIELD}) // 可以用于方法和类
 @Retention(RetentionPolicy.RUNTIME) // 在运行时可用
 public @interface ExceptionCode {
 
--- integration-base/src/main/java/com/jxp/integration/response/ResultCode.java
+++ integration-base/src/main/java/com/jxp/integration/response/ResultCode.java
@@ -5,4 +5,12 @@ package com.jxp.integration.response;
  * Created on 2024-11-19 10:58
  */
 public interface ResultCode {
+
+    @ExceptionCode(
+            code = 0,
+            message = "OK",
+            zhCN = "成功",
+            enUS = "ok"
+    )
+    int OK = 0;
 }
